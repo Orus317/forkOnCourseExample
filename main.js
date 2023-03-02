5ced850c4f405c18038f13001a3bb8fec8c3f6ce
oriol <orioloriolpalacios@outlook.com>
2023-03-02 16:54:30 -0500

    Adding the menus

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const menuEmail = document.getElementsByClassName("navbar-email")[0];
 const desktopMenu = document.getElementsByClassName("desktop-menu")[0];
+
+const hamMenu = document.getElementById("ham-Icon");
+const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+
+const menuCar = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
+menuCar.addEventListener('click', toggleCarAside);
+
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
