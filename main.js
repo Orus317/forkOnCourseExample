39514e100ed48c8b511d4fc3f3d7bb940a36e6dc
oriol <orioloriolpalacios@outlook.com>
2023-03-03 11:35:27 -0500

    Simplification of the products render

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
 const menuCar = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector('.product-detail');
 
+const cardsContainer = document.querySelector(".cards-container");
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamMenu.addEventListener('click', toggleMobileMenu);
 menuCar.addEventListener('click', toggleCarAside);
@@ -26,3 +28,60 @@ function toggleCarAside() {
     mobileMenu.classList.add('inactive');
     aside.classList.toggle('inactive');
 }
+
+const products = [];
+const productsNodes = [];
+products.push({
+    name: "Bike",
+    price: 120,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+products.push({
+    name: "PC",
+    price: 820,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+products.push({
+    name: "Pantalla",
+    price: 200,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+products.push({
+    name: "Bike",
+    price: 120,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+products.push({
+    name: "PC",
+    price: 820,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+products.push({
+    name: "Pantalla",
+    price: 200,
+    img: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+function renderProducts(productsList) {
+    let nodes = "";
+    for (const product of productsList) {
+        const card = `
+            <div class="product-card">
+                <img src="${product.img}" alt="">
+                <div class="product-info">
+                    <div>
+                        <p>$${product.price}</p>
+                        <p>${product.name}</p>
+                    </div>
+                    <figure>
+                        <img src="./icons/bt_add_to_cart.svg" alt="">
+                    </figure>
+                </div>
+            </div>`;
+        nodes += card;
+        }
+    cardsContainer.innerHTML = nodes;
+}
+
+renderProducts(products);
